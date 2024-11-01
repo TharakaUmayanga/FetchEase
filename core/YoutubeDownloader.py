5a045f17474286b54f60ba1f92ec811025f7c0ee
Tharaka <tharakau@gmail.com>
2024-11-01 14:55:02 +0530

    Added Download Path

--- core/YoutubeDownloader.py
+++ core/YoutubeDownloader.py
@@ -1,10 +1,9 @@
 from yt_dlp import YoutubeDL
 
-
 class YoutubeDownloader:
-    def __init__(self):
+    def __init__(self, download_path):
+        self.download_path = download_path
         self.ydl_opts = {}
-
 
     def _set_video_quality(self, quality):
         video_quality = {
@@ -16,32 +15,23 @@ class YoutubeDownloader:
         }
         return video_quality[quality]
 
-
     def download_playlist(self, playlist_url, quality='best'):
-        # Set format option based on chosen quality
         ydl_opts = {
-            'format': self._set_video_quality(quality),  # Fallback to 'best' if quality is not in the dictionary
-            'noplaylist': False,  # Ensure downloading entire playlist
-            'outtmpl': '%(title)s.%(ext)s',
+            'format': self._set_video_quality(quality),
+            'noplaylist': False,
+            'outtmpl': f'{self.download_path}/%(title)s.%(ext)s',
             'progress_hooks': [self._progress_hook]
         }
-
-        # Initialize the downloader with the options
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([playlist_url])
 
-
     def download_video(self, video_url, quality='best'):
-        # Set format option based on chosen quality
         ydl_opts = {
             'format': self._set_video_quality(quality),
-            # Fallback to 'best' if quality is not in the dictionary
-            'noplaylist': True,  # Ensures only a single video is downloaded, even if a playlist URL is provided
-            'outtmpl': '%(title)s.%(ext)s',
+            'noplaylist': True,
+            'outtmpl': f'{self.download_path}/%(title)s.%(ext)s',
             'progress_hooks': [self._progress_hook]
         }
-
-        # Initialize the downloader with the options
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
 
@@ -52,26 +42,22 @@ class YoutubeDownloader:
             print("Download completed! Processing...")
 
     def download_audio(self, video_url, quality='best'):
-        # Set format option to audio only based on chosen quality
         audio_quality = {
             'best': 'bestaudio/best',
-            'high': 'bestaudio[abr>128]/best',  # Audio bitrate > 128 kbps
-            'medium': 'bestaudio[abr<=128]/best',  # Audio bitrate <= 128 kbps
+            'high': 'bestaudio[abr>128]/best',
+            'medium': 'bestaudio[abr<=128]/best',
             'low': 'worstaudio'
         }
         ydl_opts = {
             'format': audio_quality.get(quality, 'bestaudio'),
-            # Fallback to 'bestaudio' if quality not in the dictionary
-            'noplaylist': True,  # Ensures only a single video is processed
-            'outtmpl': '%(title)s.%(ext)s',  # Customize output file name using video title
-            'progress_hooks': [self._progress_hook],  # Add the progress hook function
-            'postprocessors': [{  # Convert audio to mp3 format after download
+            'noplaylist': True,
+            'outtmpl': f'{self.download_path}/%(title)s.%(ext)s',
+            'progress_hooks': [self._progress_hook],
+            'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
-                'preferredquality': '192',  # Set bitrate for mp3
+                'preferredquality': '192',
             }],
         }
-
-        # Initialize the downloader with the options
         with YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
